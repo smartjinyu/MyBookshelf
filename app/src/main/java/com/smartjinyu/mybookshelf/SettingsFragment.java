46e69474f122acabd4c82d81c68c56b30c2b1034
smartjinyu <smartjinyu@gmail.com>
2017-02-17 15:58:26 +0800

    finish backup location chooser

--- app/src/main/java/com/smartjinyu/mybookshelf/SettingsFragment.java
+++ app/src/main/java/com/smartjinyu/mybookshelf/SettingsFragment.java
@@ -1,8 +1,11 @@
 package com.smartjinyu.mybookshelf;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
@@ -15,10 +18,9 @@ import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.widget.Toast;
 
-import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
-import com.google.zxing.ResultPoint;
-import com.google.zxing.common.reedsolomon.GenericGF;
+import com.nononsenseapps.filepicker.FilePickerActivity;
+import com.nononsenseapps.filepicker.FilePickerFragment;
 import com.smartjinyu.mybookshelf.database.BookBaseHelper;
 
 import java.io.BufferedInputStream;
@@ -28,9 +30,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -40,29 +40,44 @@ import java.util.zip.ZipOutputStream;
  * Created by smartjinyu on 2017/2/8.
  */
 
-public class SettingsFragment extends PreferenceFragment {
+public class SettingsFragment extends PreferenceFragment{
     private static final String TAG = "SettingsFragment";
 
     private static final int STORAGE_PERMISSION = 2;
+    private static final int FOLDER_CHOOSER = 3;
 
     private Preference backupLocationPreference;
     private Preference backupPreference;
     private Preference restorePreference;
 
+    private SharedPreferences sharedPreferences;
+
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.settings_preference);
+
         backupLocationPreference = findPreference("settings_pref_backup_location");
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
-        String path = sharedPreferences.getString("settings_pref_backup_location",
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        final String path = sharedPreferences.getString("settings_pref_backup_location",
                 Environment.getExternalStorageDirectory().getAbsolutePath()+"/backups");
         backupLocationPreference.setSummary(path);
         backupLocationPreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick(Preference preference) {
-                // // TODO: 2017/2/15
-                return false;
+                if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    FragmentCompat.requestPermissions(SettingsFragment.this,
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, STORAGE_PERMISSION);
+                    return false;
+                } else {
+                    Intent i = new Intent(getActivity(), FilePickerActivity.class);
+                    i.putExtra(FilePickerActivity.EXTRA_ALLOW_CREATE_DIR,true);
+                    i.putExtra(FilePickerActivity.EXTRA_MODE,FilePickerActivity.MODE_DIR);
+                    i.putExtra(FilePickerActivity.EXTRA_START_PATH,path);
+                    startActivityForResult(i,FOLDER_CHOOSER);
+                    return true;
+                }
             }
         });
 
@@ -85,6 +100,7 @@ public class SettingsFragment extends PreferenceFragment {
         restorePreference = findPreference("settings_pref_restore");
 
     }
+
 
     private class backupTask extends AsyncTask<Void,Void,Boolean>{
         private MaterialDialog mDialog;
@@ -119,7 +135,7 @@ public class SettingsFragment extends PreferenceFragment {
                 fileName.add(getActivity().getFilesDir().getParent()+ "/shared_prefs/" + BookShelfLab.PreferenceName + ".xml");
                 fileName.add(getActivity().getFilesDir().getParent() + "/shared_prefs/" + LabelLab.PreferenceName + ".xml");
                 fileName.add(getActivity().getFilesDir().getParent() + "/shared_prefs/" + getActivity().getPackageName() + "_preferences.xml");
-                zipFile = backupLocationPreference.getSummary() + "/Bookshelf_backup_" + Calendar.getInstance().getTimeInMillis() + ".zip";
+                zipFile = backupLocationPreference.getSummary() + "/Bookshelf_backup_" + BuildConfig.VERSION_CODE + "_" + Calendar.getInstance().getTimeInMillis() + ".zip";
                 try{
                     zipFiles(fileName.toArray(new String[0]),zipFile);
                     Log.i(TAG,"Backup created " + zipFile);
@@ -230,5 +246,20 @@ public class SettingsFragment extends PreferenceFragment {
                     new backupTask().execute();
                 }
         }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode,int resultCode,Intent intent){
+        if(requestCode == FOLDER_CHOOSER && resultCode == Activity.RESULT_OK){
+            Uri uri = intent.getData();
+            File file = com.nononsenseapps.filepicker.Utils.getFileForUri(uri);
+            if(backupLocationPreference!=null){
+                String path = file.getAbsolutePath();
+                backupLocationPreference.setSummary(path);
+                sharedPreferences.edit().putString("settings_pref_backup_location",path).apply();
+                Log.i(TAG,"Change backup path to " + path);
+            }
+        }
+        super.onActivityResult(requestCode,resultCode,intent);
     }
 }

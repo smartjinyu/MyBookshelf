d7c0598a80aeeb4d202b7d1f5c366a5519cae667
smartjinyu <smartjinyu@gmail.com>
2017-02-17 17:24:14 +0800

    finish restore function (untest)

--- app/src/main/java/com/smartjinyu/mybookshelf/SettingsFragment.java
+++ app/src/main/java/com/smartjinyu/mybookshelf/SettingsFragment.java
@@ -12,6 +12,7 @@ import android.os.Environment;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v13.app.FragmentCompat;
 import android.support.v4.content.ContextCompat;
@@ -20,7 +21,6 @@ import android.widget.Toast;
 
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.nononsenseapps.filepicker.FilePickerActivity;
-import com.nononsenseapps.filepicker.FilePickerFragment;
 import com.smartjinyu.mybookshelf.database.BookBaseHelper;
 
 import java.io.BufferedInputStream;
@@ -29,10 +29,12 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 /**
@@ -43,8 +45,10 @@ import java.util.zip.ZipOutputStream;
 public class SettingsFragment extends PreferenceFragment{
     private static final String TAG = "SettingsFragment";
 
-    private static final int STORAGE_PERMISSION = 2;
-    private static final int FOLDER_CHOOSER = 3;
+    private static final int STORAGE_PERMISSION_LOACTION = 1;
+    private static final int STORAGE_PERMISSION_BACKUP = 2;
+    private static final int STORAGE_PERMISSION_RESTORE = 3;
+    private static final int FOLDER_CHOOSER = 4;
 
     private Preference backupLocationPreference;
     private Preference backupPreference;
@@ -68,7 +72,7 @@ public class SettingsFragment extends PreferenceFragment{
                 if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
                         != PackageManager.PERMISSION_GRANTED) {
                     FragmentCompat.requestPermissions(SettingsFragment.this,
-                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, STORAGE_PERMISSION);
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, STORAGE_PERMISSION_LOACTION);
                     return false;
                 } else {
                     Intent i = new Intent(getActivity(), FilePickerActivity.class);
@@ -89,7 +93,7 @@ public class SettingsFragment extends PreferenceFragment{
                 if(ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
                         != PackageManager.PERMISSION_GRANTED){
                     FragmentCompat.requestPermissions(SettingsFragment.this,
-                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},STORAGE_PERMISSION);
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, STORAGE_PERMISSION_BACKUP);
                 }else{
                     new backupTask().execute();
                 }
@@ -98,6 +102,19 @@ public class SettingsFragment extends PreferenceFragment{
         });
 
         restorePreference = findPreference("settings_pref_restore");
+        restorePreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                if(ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED){
+                    FragmentCompat.requestPermissions(SettingsFragment.this,
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, STORAGE_PERMISSION_BACKUP);
+                }else{
+
+                }
+                return false;
+            }
+        });
 
     }
 
@@ -224,6 +241,167 @@ public class SettingsFragment extends PreferenceFragment{
 
     }
 
+    private class restoreTask extends AsyncTask<String,Void,Boolean>{
+        private MaterialDialog mDialog;
+        @Override
+        protected void onPreExecute(){
+            mDialog = new MaterialDialog.Builder(getActivity())
+                    .title(R.string.restore_progress_dialog_title)
+                    .content(R.string.backup_progress_dialog_content)
+                    .progress(true,0)
+                    .progressIndeterminateStyle(false)
+                    .show();
+
+        }
+
+        @Override
+        protected Boolean doInBackground(String...params){
+            String backupFile = params[0];
+            String unZipDir = backupFile.substring(0,backupFile.lastIndexOf("."));
+            try{
+                unzip(backupFile,unZipDir);
+            }catch (IOException e){
+                Log.e(TAG,"Unzip failed 1, ioe = " +e.toString());
+                return false;
+            }
+            String[] fileName = new File(unZipDir).list();
+            for(String file: fileName){
+                // restore files
+                if(file.equals("Covers.zip")){
+                    try {
+                        unzip(unZipDir + "/Covers.zip",unZipDir + "/Covers");
+                        File srcCoverFolder = new File(unZipDir + "/Covers");
+                        File[] covers = srcCoverFolder.listFiles();
+                        if(getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES)!=null){
+                            String destCoverFolder = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES).getAbsolutePath();
+                            File destCover = new File(destCoverFolder);
+                            destCover.delete();
+                            for(File cover: covers){
+                                copyFile(cover,new File(destCoverFolder + "/" + cover.getName()));
+                            }
+                        }
+                    }catch (IOException e){
+                        Log.e(TAG,"Unzip failed 2, ioe = " +e.toString());
+                        return false;
+                    }
+
+                    continue;
+                }
+                if(file.equals(BookShelfLab.PreferenceName + ".xml")){
+                    String src = unZipDir + "/" + BookShelfLab.PreferenceName + ".xml";
+                    String dest = getActivity().getFilesDir().getParent()+ "/shared_prefs/" + BookShelfLab.PreferenceName + ".xml";
+                    copyFile(new File(src),new File(dest));
+                    continue;
+                }
+
+                if(file.equals(LabelLab.PreferenceName + ".xml")){
+                    String src = unZipDir + "/" + LabelLab.PreferenceName + ".xml";
+                    String dest = getActivity().getFilesDir().getParent()+ "/shared_prefs/" + LabelLab.PreferenceName + ".xml";
+                    copyFile(new File(src),new File(dest));
+                    continue;
+                }
+
+                if(file.equals(getActivity().getPackageName() + "_preferences.xml")){
+                    String src = unZipDir + "/" + getActivity().getPackageName() + "_preferences.xml";
+                    String dest = getActivity().getFilesDir().getParent()+ getActivity().getPackageName() + "_preferences.xml";
+                    copyFile(new File(src),new File(dest));
+                    continue;
+                }
+
+                if(file.equals(BookBaseHelper.DATABASE_NAME + ".db")){
+                    String src = unZipDir + "/" + BookBaseHelper.DATABASE_NAME + "db.xml";
+                    String dest = getActivity().getDatabasePath(BookBaseHelper.DATABASE_NAME).getAbsolutePath();
+                    copyFile(new File(src),new File(dest));
+                }
+            }
+
+
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean isSucceed){
+            mDialog.dismiss();
+            if(isSucceed){
+                Toast.makeText(getActivity(),getString(R.string.restore_succeed_toast),Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(getActivity(),getString(R.string.restore_fail_toast),Toast.LENGTH_LONG).show();
+            }
+        }
+
+        //zip and unzip code is referenced to http://stackoverflow.com/questions/7485114/how-to-zip-and-unzip-the-files
+        private void unzip(String zipFile,String location) throws IOException{
+            int size;
+            int BUFFER_SIZE = 2048;
+            byte[] buffer = new byte[BUFFER_SIZE];
+            if(!location.endsWith("/")){
+                location += "/";
+            }
+            File file = new File(location);
+            if(!file.isDirectory()){
+                boolean result = file.mkdirs();
+            }
+            try(ZipInputStream zipInputStream = new ZipInputStream(new BufferedInputStream(new FileInputStream(zipFile)))){
+                ZipEntry ze = null;
+                while((ze = zipInputStream.getNextEntry())!=null){
+                    String path = location + ze.getName();
+                    File unzipFile = new File(path);
+                    if(ze.isDirectory()){
+                        if(!unzipFile.isDirectory()){
+                            boolean result = unzipFile.mkdirs();
+                        }
+                    }else{
+                        // check and create parent
+                        File parentDir = unzipFile.getParentFile();
+                        if(parentDir!=null){
+                            if(!parentDir.isDirectory()){
+                                boolean result = parentDir.mkdirs();
+                            }
+                        }
+
+                        //unzip the file
+                        FileOutputStream out = new FileOutputStream(unzipFile,false);
+                        BufferedOutputStream fout = new BufferedOutputStream(out,BUFFER_SIZE);
+                        try{
+                            while((size = zipInputStream.read(buffer,0,BUFFER_SIZE)) != -1){
+                                fout.write(buffer,0,size);
+                            }
+                            zipInputStream.closeEntry();
+                        }finally {
+                            fout.flush();
+                            fout.close();
+                        }
+                    }
+                }
+            }
+
+        }
+
+        // http://stackoverflow.com/questions/9292954/how-to-make-a-copy-of-a-file-in-android
+        private boolean copyFile(File src, File dst)  {
+            if(!dst.getParentFile().exists()){
+                dst.getParentFile().mkdirs();
+            }
+            if(dst.exists()){
+                dst.delete();
+            }
+
+            try(FileInputStream inStream = new FileInputStream(src);
+                FileOutputStream outStream = new FileOutputStream(dst)) {
+
+                FileChannel inChannel = inStream.getChannel();
+                FileChannel outChannel = outStream.getChannel();
+                inChannel.transferTo(0, inChannel.size(), outChannel);
+                return true;
+            }catch (IOException e){
+                Log.e(TAG,"Copy file IOException = " + e.toString() + ",src = " + src.getAbsolutePath() + ",dest = " + dst.getAbsolutePath());
+                return false;
+            }
+
+        }
+
+    }
+
 
 
 
@@ -235,16 +413,38 @@ public class SettingsFragment extends PreferenceFragment{
             @NonNull String permissions[],
             @NonNull int[] grantResults){
 
-        switch (RequestCode){
-            case STORAGE_PERMISSION:
-                if(!(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)){
+        switch (RequestCode) {
+            case STORAGE_PERMISSION_LOACTION:
+                if (!(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
                     // permission not granted
-                    Toast.makeText(getActivity(),getString(R.string.storage_permission_toast),Toast.LENGTH_SHORT)
+                    Toast.makeText(getActivity(), getString(R.string.storage_permission_toast1), Toast.LENGTH_SHORT)
                             .show();
-                    Log.e(TAG,"Storage Permission Denied");
-                }else{
+                    Log.e(TAG, "Storage Permission Denied 1");
+                }
+                break;
+
+            case STORAGE_PERMISSION_BACKUP:
+                if (!(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // permission not granted
+                    Toast.makeText(getActivity(), getString(R.string.storage_permission_toast2), Toast.LENGTH_SHORT)
+                            .show();
+                    Log.e(TAG, "Storage Permission Denied 2");
+                } else {
                     new backupTask().execute();
                 }
+                break;
+            case STORAGE_PERMISSION_RESTORE:
+                if (!(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // permission not granted
+                    Toast.makeText(getActivity(), getString(R.string.storage_permission_toast3), Toast.LENGTH_SHORT)
+                            .show();
+                    Log.e(TAG, "Storage Permission Denied 3");
+                }else{
+
+                }
+                break;
+
+
         }
     }
 

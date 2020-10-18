e2618cd0f2656ba5669da23bcaf21143ce79c9d9
smartjinyu <smartjinyu@gmail.com>
2020-10-18 00:52:44 -0700

    fix a bug occured when custom cover image name is too short

--- compressor/src/main/java/id/zelory/compressor/FileUtil.java
+++ compressor/src/main/java/id/zelory/compressor/FileUtil.java
@@ -35,7 +35,7 @@ public class FileUtil {
         InputStream inputStream = context.getContentResolver().openInputStream(uri);
         String fileName = getFileName(context, uri);
         String[] splitName = splitFileName(fileName);
-        File tempFile = File.createTempFile(splitName[0], splitName[1]);
+        File tempFile = File.createTempFile(splitName[0] + "_temp", splitName[1]);
         tempFile = rename(tempFile, fileName);
         tempFile.deleteOnExit();
         FileOutputStream out = null;
